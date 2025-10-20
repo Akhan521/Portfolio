d03fba1f3f36e645b5bec61d3622dc79fc0182c3
Aamir Khan <aamirksfg@gmail.com>
2025-10-20 14:05:03 -0700

    updating resume link

--- components/hero.tsx
+++ components/hero.tsx
@@ -48,7 +48,7 @@ export function Hero() {
               View My Work
             </Button>
             <a
-              href="https://drive.google.com/file/d/1avfq7rcr1CGIo7Ju3_FDgi_iLT4p-1Se/view?usp=sharing"
+              href="https://drive.google.com/file/d/1M8G1j07T06Zy8hvqql0er00shjVmfVIv/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
             >
